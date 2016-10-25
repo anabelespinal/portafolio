3ff60eb8bf1e0c47a377e00561c323bb0aa01aa9
anbelespinal <anabelespinal>
2016-10-24 19:49:00 -0500

    iniciando proyecto

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// menu desplegable
+$(".button-collapse").sideNav();
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+// parallax header
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//     var i;
+//     var x = document.querySelectorAll("#banner div img");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1}
+//     x[slideIndex-1].style.display = "block";
+//     setTimeout(carousel, 3500);
+// }
