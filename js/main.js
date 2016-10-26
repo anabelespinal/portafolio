762b1a65e005895bc680e5fffade1138733732ee
anbelespinal <anabelespinal>
2016-10-25 19:57:54 -0500

    footer and button up

--- js/main.js
+++ js/main.js
@@ -8,17 +8,38 @@ $(document).ready(function(){
 // $(document).ready(function(){
 //   $('.parallax').parallax();
 // });
-// var slideIndex = 0;
-// carousel();
 
-// function carousel() {
-//     var i;
-//     var x = document.querySelectorAll("#banner div img");
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > x.length) {slideIndex = 1}
-//     x[slideIndex-1].style.display = "block";
-//     setTimeout(carousel, 3500);
-// }
+// slider
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.querySelectorAll(".heading div h1");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 3500);
+}
+
+
+// boton arriba
+
+$(document).ready(function(){
+	$("#boton-subir").click(function(){
+		$("body, html").animate({
+			scrollTop: "0px"
+		}, 300);
+	});
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > 650){
+			$("#boton-subir").slideDown(300);
+		}else{
+			$("#boton-subir").slideUp(300);
+		}
+	});
+});
